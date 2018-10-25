87c140c6d253cc5e7fed9f72e70a9bbedee8d32f
Reid McIlroy-Young <reidmcy@gmail.com>
2018-10-25 05:34:58 -0400

    looks like the years are slightly different

--- data_processing/createboardMap.py
+++ data_processing/createboardMap.py
@@ -7,12 +7,14 @@ outputPath = '../data/mapping_2018-09.csv'
 def main():
     games = imitation_chess.GamesFile(gamesPath)
     with open(outputPath, 'w') as f:
-        f.write("board,move,game\n")
+        f.write("board,move,game,BlackElo,WhiteElo\n")
         for i, g in enumerate(games):
             d = imitation_chess.getBoardMoveMap(g)
-            gameID = g.headers['LichessId']
+            gameID = g.headers['Site'].split('/')[-1]
+            BlackElo = g.headers['BlackElo']
+            WhiteElo = g.headers['WhiteElo']
             for k, v in d.items():
-                f.write(f"{k},{v},{gameID}\n")
+                f.write(f"{k},{v},{gameID},{BlackElo},{WhiteElo}\n")
             if i % 1000 == 0:
                 print(f"{i}: {gameID}")
                 f.flush()
