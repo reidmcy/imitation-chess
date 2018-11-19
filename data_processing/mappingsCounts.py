cb9e109fe8a28a73b532c53e37e4b600098523a6
Reid McIlroy-Young <reidmcy@gmail.com>
2018-11-19 13:58:18 -0600

    Starting to process the mappings

---
+++ data_processing/mappingsCounts.py
@@ -0,0 +1,47 @@
+import re
+import chess
+import sys
+import collections
+import json
+
+target = sys
+
+outputSuffix = '_collected'
+
+minCount = 50
+
+def writeBoard(boards, path, count):
+    with open(path, 'w') as f:
+        for b, d in boards.items():
+            if sum(d.values()) >= count:
+                json.dump({'board' : b, 'counts' : d}, f)
+
+
+def processMapping(target):
+    dirname, filename = os.path.split(os.path.splitext(target)[0])
+    outputName = os.path.join(dirname, f"{filename}{outputSuffix}.json")
+
+    with open(target) as fin:
+        reader = csv.DictReader(fin)
+        counts = collections.defaultdict(dict)
+        for i, l in enumerate(reader):
+            try:
+                counts[l['board']][l['move']] += 1
+            except KeyError:
+                counts[l['board']] = {l['move'] : 1}
+            if i % 10000:
+                print(f"{i}: boards {len(counts)}", end = '\r')
+            if i % 1000000:
+                writeBoard(counts, outputName, minCount)
+    print('\nDone')
+
+
+def main():
+    for targetPath in sys.argv[1:]:
+        try:
+            processMapping(targetPath)
+        except FileExistsError:
+            print("Skipping: ", targetPath)
+
+if __name__ == '__main__':
+    main()
