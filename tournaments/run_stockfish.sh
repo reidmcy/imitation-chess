5b88c6afd1cec5671558a93b294081506430b632
Reid McIlroy-Young <reidmcy@gmail.com>
2018-11-19 04:32:05 -0600

    Timings

--- tournaments/run_stockfish.sh
+++ tournaments/run_stockfish.sh
@@ -22,6 +22,6 @@ for engine2Level in 0 5 10 15 20;
      -pgnout "stockfish_games/${1}/hai-${engine1}-v-stockfish-${engine2Level}.pgn" \
      -engine name="hai-${engine1}" cmd=lc0 arg="--threads=1" arg="--noise" arg="--weights=${weightsDir}/${engine1}-64x6-140000.pb.gz" arg="--no-ponder"\
      -engine name="stockfish-${engine2Level}" cmd=stockfish initstr="setoption name Ponder value False\nsetoption name Ponder value False\nsetoption name Skill Level value ${engine2Level}"\
-     -each proto=uci tc=40/40
+     -each proto=uci tc=$time
 
 done
