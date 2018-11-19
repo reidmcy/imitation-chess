d689f8a6153a96168f77b3bc5a2889d09ed44f10
Reid McIlroy-Young <reidmcy@mail.cs.toronto.edu>
2018-11-19 04:07:13 -0500

    Got the cute chess working

--- tournaments/run.sh
+++ tournaments/run.sh
@@ -12,6 +12,7 @@ weightsDir="/w/225/reidmcy/chess/imitation-chess/networks"
 
 cutechess-cli -rounds 100 -tournament gauntlet -concurrency 4 \
  -pgnout "hai-${engine1}-v-hai-${engine2}.pgn" \
- -engine name="hai-${engine1}" cmd=lc0 arg="--threads=1" arg="--noise" arg="--weights=${weightsDir}/${engine1}-64x6-140000.pb.gz" arg="--playouts=800" arg="--noponder"\
- -engine name="hai-${engine2}" cmd=lc0 arg="--threads=1" arg="--noise" arg="--weights=${weightsDir}/${engine2}-64x6-140000.pb.gz" arg="--playouts=800" arg="--noponder"\
- -each proto=uci tc=inf
+ -engine name="hai-${engine1}" cmd=lc0 arg="--threads=1" arg="--noise" arg="--weights=${weightsDir}/${engine1}-64x6-140000.pb.gz" arg="--no-ponder"\
+ -engine name="hai-${engine2}" cmd=lc0 arg="--threads=1" arg="--noise" arg="--weights=${weightsDir}/${engine2}-64x6-140000.pb.gz" arg="--no-ponder"\
+ -each proto=uci tc=40/40
+
