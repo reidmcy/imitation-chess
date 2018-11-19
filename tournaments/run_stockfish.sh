16af82692acace9c5b118372c28e5b46d50b59eb
Reid McIlroy-Young <reidmcy@gmail.com>
2018-11-19 04:25:30 -0600

    Going to stockfish gauntlet

--- tournaments/run_stockfish.sh
+++ tournaments/run_stockfish.sh
@@ -3,15 +3,25 @@
 # Add -debug to see engine output
 
 engine1="${1}" #lc0
-engine2="${2}" #stockfish
+#engine2="${2}" #stockfish
+times="10:00/40"
 
 weightsDir="/w/225/reidmcy/chess/imitation-chess/networks"
 
 
-#No time control, but only look at 800 new nodes a round
+mkdir -p stockfish_games/
 
-cutechess-cli -rounds 100 -tournament gauntlet -concurrency 4 -event "hai ${1} v stockfish ${2}"\
- -pgnout "hai-${engine1}-v-stockfish-${engine2}.pgn" \
- -engine name="hai-${engine1}" cmd=lc0 arg="--threads=1" arg="--noise" arg="--weights=${weightsDir}/${engine1}-64x6-140000.pb.gz" arg="--no-ponder"\
- -engine name="stockfish-${engine2}" cmd=stockfish initstr="setoption name Ponder value False\nsetoption name Ponder value False\nsetoption name Skill Level value ${engine2}"\
- -each proto=uci tc=40/40
+mkdir -p "stockfish_games/${1}"
+
+for engine2Level in 0 5 10 15 20;
+    do
+
+    echo $engine2Level
+
+    cutechess-cli -rounds 100 -tournament gauntlet -concurrency 4 -event "hai ${1} v stockfish ${2}"\
+     -pgnout "stockfish_games/${1}/hai-${engine1}-v-stockfish-${engine2Level}.pgn" \
+     -engine name="hai-${engine1}" cmd=lc0 arg="--threads=1" arg="--noise" arg="--weights=${weightsDir}/${engine1}-64x6-140000.pb.gz" arg="--no-ponder"\
+     -engine name="stockfish-${engine2Level}" cmd=stockfish initstr="setoption name Ponder value False\nsetoption name Ponder value False\nsetoption name Skill Level value ${engine2Level}"\
+     -each proto=uci tc=40/40
+
+done
