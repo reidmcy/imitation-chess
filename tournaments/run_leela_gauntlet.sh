9ffaaf21e9ec9228ce4e09831fe2ae1aae9725fb
Reid McIlroy-Young <reidmcy@gmail.com>
2018-11-19 04:16:46 -0600

    Tweaks

--- tournaments/run_leela_gauntlet.sh
+++ tournaments/run_leela_gauntlet.sh
@@ -13,6 +13,8 @@ leelasDir="/w/225/reidmcy/chess/imitation-chess/networks/leela_weights"
 
 mkdir -p leela_games/
 
+mkdir -p "leela_games/${1}"
+
 for engine2Path in /w/225/reidmcy/chess/imitation-chess/networks/leela_weights/*.gz;
 do
     echo $engine2Path
@@ -20,7 +22,7 @@ do
     echo $engine2Name
 
     cutechess-cli -rounds 100 -tournament gauntlet -concurrency 4 -event "hai ${1} v leela ${engine2Name}"\
-     -pgnout "leela_games/hai-${engine1}-v-leela-${engine2Name}.pgn" \
+     -pgnout "leela_games/${1}/hai-${engine1}-v-leela-${engine2Name}.pgn" \
      -engine name="hai-${engine1}" cmd=lc0 arg="--threads=1" arg="--noise" arg="--weights=${weightsDir}/${engine1}-64x6-140000.pb.gz" arg="--no-ponder"\
      -engine name="leela-${engine2Name}" cmd=lc0 arg="--threads=1" arg="--noise" arg="--weights=${engine2Path}" arg="--no-ponder"\
      -each proto=uci tc="${times}"
