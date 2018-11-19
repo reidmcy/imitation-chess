7af91ed31f8d403d019b6c8abc5f5be43f607a40
Reid McIlroy-Young <reidmcy@gmail.com>
2018-11-19 04:15:49 -0600

    Tweaks

--- tournaments/run_leela_gauntlet.sh
+++ tournaments/run_leela_gauntlet.sh
@@ -3,6 +3,7 @@
 # Add -debug to see engine output
 
 engine1="${1}"
+times="10:00/49"
 
 weightsDir="/w/225/reidmcy/chess/imitation-chess/networks"
 
@@ -10,19 +11,17 @@ leelasDir="/w/225/reidmcy/chess/imitation-chess/networks/leela_weights"
 
 #No time control, but only look at 800 new nodes a round
 
+mkdir -p leela_games/
+
 for engine2Path in /w/225/reidmcy/chess/imitation-chess/networks/leela_weights/*.gz;
 do
+    echo $engine2Path
+    engine2Name=`python -c "import os.path;print(os.path.basename('${engine2Path}').split('.')[0])"`
+    echo $engine2Name
 
-echo $engine2Path
-
-engine2Name=`python -c "import os.path;print(os.path.basename('${engine2Path}').split('.')[0])"`
-
-echo $engine2Name
-
-cutechess-cli -rounds 100 -tournament gauntlet -concurrency 4 -event "hai ${1} v hai ${2}"\
- -pgnout "hai-${engine1}-v-leela-${engine2Name}.pgn" \
- -engine name="hai-${engine1}" cmd=lc0 arg="--threads=1" arg="--noise" arg="--weights=${weightsDir}/${engine1}-64x6-140000.pb.gz" arg="--no-ponder"\
- -engine name="leela-${engine2Name}" cmd=lc0 arg="--threads=1" arg="--noise" arg="--weights=${engine2Path}" arg="--no-ponder"\
- -each proto=uci tc=40/40
-
+    cutechess-cli -rounds 100 -tournament gauntlet -concurrency 4 -event "hai ${1} v leela ${engine2Name}"\
+     -pgnout "leela_games/hai-${engine1}-v-leela-${engine2Name}.pgn" \
+     -engine name="hai-${engine1}" cmd=lc0 arg="--threads=1" arg="--noise" arg="--weights=${weightsDir}/${engine1}-64x6-140000.pb.gz" arg="--no-ponder"\
+     -engine name="leela-${engine2Name}" cmd=lc0 arg="--threads=1" arg="--noise" arg="--weights=${engine2Path}" arg="--no-ponder"\
+     -each proto=uci tc="${times}"
  done
