4bec9f0ebafc42c29e1009f1fee3ce6f4be5aa97
Reid McIlroy-Young <reidmcy@gmail.com>
2018-11-19 02:47:07 -0600

    Trying glinscott method

---
+++ tournaments/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Add -debug to see engine output
+
+engine1=1100
+engine2=1200
+
+weightsDir="/Users/Reid/Documents/School/Toronto/lab/Chess/imitation_chess/networks/"
+
+
+#No time control, but only look at 800 new nodes a round
+
+cutechess-cli -rounds 100 -tournament gauntlet -concurrency 4 \
+ -pgnout "hai-${engine1}-v-hai-${engine2}.pgn" \
+ -engine name="hai-${engine1}" cmd=lc0 arg="--threads=1" arg="--noise" arg="--weights=${weightsDir}/${engine1}-64x6-140000.pb.gz" arg="--playouts=800" arg="--noponder"\
+ -engine name="hai-${engine2}" cmd=lc0 arg="--threads=1" arg="--noise" arg="--weights=${weightsDir}/${engine2}-64x6-140000.pb.gz" arg="--playouts=800" arg="--noponder"\
+ -each proto=uci tc=inf
+
+mv -v results.pgn "$NXT-vs-$CUR.pgn"
