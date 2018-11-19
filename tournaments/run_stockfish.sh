cb5b0949cedaa7d84028eaea7928e14043f4a1a0
Reid McIlroy-Young <reidmcy@gmail.com>
2018-11-19 03:22:58 -0600

    stetting up stockfish tourneys

---
+++ tournaments/run_stockfish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Add -debug to see engine output
+
+engine1=1100 #lc0
+engine2=0 #stockfish
+
+weightsDir="/w/225/reidmcy/chess/imitation-chess/networks"
+
+
+#No time control, but only look at 800 new nodes a round
+
+cutechess-cli -rounds 100 -tournament gauntlet -concurrency 4 \
+ -pgnout "hai-${engine1}-v-hai-${engine2}.pgn" \
+ -engine name="hai-${engine1}" cmd=lc0 arg="--threads=1" arg="--noise" arg="--weights=${weightsDir}/${engine1}-64x6-140000.pb.gz" arg="--no-ponder"\
+ -engine name="stockfish-${engine2}" cmd=stockfish initstr="setoption name Ponder value False\nsetoption name Ponder value False\nsetoption name Skill Level value ${engine2}"\
+ -each proto=uci tc=40/40
