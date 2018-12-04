aabcea4e9a5f94fac2726c3d2b333ed02c42a1ac
Reid McIlroy-Young <reidmcy@gmail.com>
2018-12-04 04:29:34 -0500

    First attempt

---
+++ data_processing/run_tourney.py
@@ -0,0 +1,31 @@
+import imitation_chess
+
+import os
+import multiprocessing
+
+nProcesses = 64
+
+resultsDir = 'all_games'
+
+def main():
+
+    os.makedirs(resultsDir, exist_ok=True)
+    engines = imitation_chess.listAllEngines()
+
+    opponents = []
+    for i, e1 in enumerate(engines):
+        for e2 in engines[i:]:
+            opponents.append((e1, e2, 1))
+
+    with multiprocessing.pool(processes = nProcesses) as pool:
+        games = pool.starmap(imitation_chess.playTourney, opponents)
+        for (e1, e2, _), g in zip(opponents, games):
+            e1Name = json.loads(e1)['name']
+            e2Name = json.loads(e2)['name']
+            with open(os.path.join(resultsDir, f"{e1Name}-{e2Name}.pgn"), 'a') as f:
+                for game in games:
+                    f.write(str(g))
+                    f.write('\n')
+
+if __name__ == '__main__':
+    main()
