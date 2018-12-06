f516433370fafed50c5a32448ab5c542de83b0d7
Reid McIlroy-Young <reidmcy@gmail.com>
2018-12-06 02:23:55 -0500

    Trying only haibrids

--- data_processing/run_tourney.py
+++ data_processing/run_tourney.py
@@ -5,20 +5,27 @@ import json
 import multiprocessing
 
 nProcesses = 86
-num_games = 10
+num_games = 100
+num_leelas = 12
 
-
-resultsDir = 'all_games_ada'
+resultsDir = 'hairbrid_games_ada'
 
 def main():
 
     os.makedirs(resultsDir, exist_ok=True)
-    engines = imitation_chess.listAllEngines(leelaConfig={'movetime' : 1000, 'nodes' : 1000}, hiabridConfig = {'movetime' : 1000, 'nodes' : 1000})
+    engines = imitation_chess.listAllEngines(leelaConfig={'movetime' : 10000, 'nodes' : 10000}, hiabridConfig = {'movetime' : 10000, 'nodes' : 10000})
 
     opponents = []
     for i, e1 in enumerate(engines):
-        for e2 in engines[i:]:
-            opponents.append((e1, e2, num_games, resultsDir))
+        if 'hiabrid' in e1:
+            lcount = 0
+            for e2 in engines[i:]:
+                dat = json.loads(e2)
+                if 'leela' in e1:
+                    lcount += 1
+                    if lcount >= num_leelas:
+                        continue
+                opponents.append((e1, e2, num_games, resultsDir))
 
     #stockfish takes a while at high skill
     #opponents = opponents[::-1]
